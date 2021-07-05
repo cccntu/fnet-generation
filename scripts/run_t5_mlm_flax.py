8ec79980952c0eb08cf23311fed2a32b265d0483
Jonathan Chang <ttjtftx@gmail.com>
2021-07-05 11:31:39 +0800

    Print shape at first step

--- scripts/run_t5_mlm_flax.py
+++ scripts/run_t5_mlm_flax.py
@@ -785,6 +785,8 @@ if __name__ == "__main__":
     for info, model_inputs in training_steps_generator:
         # Model forward
         model_inputs = shard(model_inputs.data)
+        if info['step'] == 1:
+            print(jax.tree_map(lambda x:x.shape, model_inputs))
         state, train_metric, dropout_rngs = p_train_step(state, model_inputs, dropout_rngs)
         train_metric = device_get_one_shard(train_metric)
         train_metrics.append(train_metric)
